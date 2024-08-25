eebebca079f0392234926060fdfd0230a1e3ab25
Vap0r1ze <superdash993@gmail.com>
2024-08-25 19:08:52 -0400

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL,
+
   plugins: [
     vue(),
     vueDevTools(),
