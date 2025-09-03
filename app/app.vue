574d734e721dd288a494f79fee955cc826abacda
sadan <117494111+sadan4@users.noreply.github.com>
2025-09-03 17:23:04 -0400

    MY EYES

--- app/app.vue
+++ app/app.vue
@@ -5,3 +5,19 @@
     </NuxtLayout>
     <!-- </UApp> -->
 </template>
+<style>
+@layer base {
+    @media (prefers-color-scheme: dark) {
+        body {
+            background-color: #1a1b26;
+            color: #c0caf5;
+        }
+        a {
+            color: oklch(0.719 0.1322 264.2);
+            &:visited {
+                color: #9d7cd8;
+            }
+        }
+    }
+}
+</style>
